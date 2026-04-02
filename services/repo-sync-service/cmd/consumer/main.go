99da63d8d04adb8140744ea152728983445c4a73
Rohan Dave <rohandewangdave@gmail.com>
2026-04-02 16:12:05 -0700

    cleanup creation of consumers

--- services/repo-sync-service/cmd/consumer/main.go
+++ services/repo-sync-service/cmd/consumer/main.go
@@ -24,9 +24,27 @@ func main() {
 		GroupId: "repo-sync-lifecycle-consumer-group",
 	}
 
-	for i := 0; i < numberOfLifeCycleConsumers; i++ {
-		c := kafka.NewKafkaConsumer(lifecycleConsumerConfig)
-		err := c.SubscribeTopics([]string{cfg.Kafka.LifeCycleTopic})
+	createAndRunNKafkaConsumers(numberOfLifeCycleConsumers, lifecycleConsumerConfig, cfg.Kafka.LifeCycleTopic, logger)
+
+	eventConsumerConfig := &kafka.KafkaConsumerConfig{
+		Brokers: cfg.Kafka.Brokers,
+		GroupId: "repo-sync-event-consumer-group",
+	}
+
+	createAndRunNKafkaConsumers(numberOfEventConsumers, eventConsumerConfig, cfg.Kafka.EventsTopic, logger)
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+	<-stop
+
+	logger.Printf("shutting down %s consumer", cfg.ServiceName)
+}
+
+func createAndRunNKafkaConsumers(number int, config *kafka.KafkaConsumerConfig, topic string, logger *log.Logger) {
+	for i := range number {
+		c := kafka.NewKafkaConsumer(config)
+		err := c.SubscribeTopics([]string{topic})
+
 		if err != nil {
 			logger.Fatalf("failed to subscribe lifecycle consumer to topic: %v", err)
 		}
@@ -48,41 +66,4 @@ func main() {
 			}
 		}(c, i)
 	}
-
-	eventConsumerConfig := &kafka.KafkaConsumerConfig{
-		Brokers: cfg.Kafka.Brokers,
-		GroupId: "repo-sync-event-consumer-group",
-	}
-
-	for i := 0; i < numberOfEventConsumers; i++ {
-		c := kafka.NewKafkaConsumer(eventConsumerConfig)
-		err := c.SubscribeTopics([]string{cfg.Kafka.EventsTopic})
-		if err != nil {
-			logger.Fatalf("failed to subscribe event consumer to topic: %v", err)
-		}
-
-		go func(consumer kafka.Consumer, workerId int) {
-			defer consumer.Close()
-
-			for {
-				msg, err := consumer.ReadMessage(5 * time.Second)
-				if err != nil {
-					logger.Printf("events consumer %d read error: %v", workerId, err)
-					continue
-				}
-
-				if msg == nil {
-					continue
-				}
-
-				logger.Printf("events consumer %d received message: %s", workerId, string(msg.RepoURL))
-			}
-		}(c, i)
-	}
-
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
-	<-stop
-
-	logger.Printf("shutting down %s consumer", cfg.ServiceName)
 }
